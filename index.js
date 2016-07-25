18fc696c21261afa082ef1382ab30af1f02b0d8e
Adam Buczynski <me@adambuczynski.com>
2016-07-25 14:34:16 +1200

    Must return custom transform

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = function toJSON(schema) {
 
       //Call custom transform if present
       if (transform) {
-        transform(doc, ret);
+        return transform(doc, ret);
       }
     },
   };
